ee13d947b610b87054cd19862e8b10d7098c67a8
pxlin <768331153@qq.com>
2025-11-27 10:21:33 +0800

    修复重置密码失败问题

--- src/main/java/hbnu/project/zhiyanbackend/ZhiyanBackendApplication.java
+++ src/main/java/hbnu/project/zhiyanbackend/ZhiyanBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableJpaAuditing
 public class ZhiyanBackendApplication {
 
     public static void main(String[] args) {
