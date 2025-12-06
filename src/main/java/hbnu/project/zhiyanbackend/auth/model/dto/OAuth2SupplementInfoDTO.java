ab66af290c72bfa9ffee559a29c01cea5271c60e
ErguTree <zjm88822201@126.com>
2025-12-06 17:52:17 +0800

    密码强度认证添加后拉取

--- src/main/java/hbnu/project/zhiyanbackend/auth/model/dto/OAuth2SupplementInfoDTO.java
+++ src/main/java/hbnu/project/zhiyanbackend/auth/model/dto/OAuth2SupplementInfoDTO.java
@@ -3,6 +3,7 @@ package hbnu.project.zhiyanbackend.auth.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
