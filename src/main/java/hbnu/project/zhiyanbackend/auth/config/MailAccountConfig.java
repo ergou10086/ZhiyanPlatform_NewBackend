c85ad8f1a7fa05739725c1d2f154afedb2e1c7d2
ErguTree <zjm88822201@126.com>
2026-01-09 16:39:13 +0800

    修复最后存在的一些bug，然后提升了一下性能

--- src/main/java/hbnu/project/zhiyanbackend/auth/config/MailAccountConfig.java
+++ src/main/java/hbnu/project/zhiyanbackend/auth/config/MailAccountConfig.java
@@ -2,30 +2,24 @@ package hbnu.project.zhiyanbackend.auth.config;
 
 import cn.hutool.extra.mail.MailAccount;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Properties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
- * 邮件账户配置类
- * 将 Spring Boot 的邮件配置转换为 Hutool 的 MailAccount Bean
- *
+ * Hutool MailAccount 配置类
+ * 用于解决 MailUtils 静态初始化时需要的 MailAccount Bean
+ * 
  * @author ErgouTree
  */
 @Slf4j
-@AutoConfiguration
-@ConditionalOnProperty(prefix = "spring.mail", name = "host")
+@Configuration
 public class MailAccountConfig {
 
     @Value("${spring.mail.host}")
     private String host;
 
-    @Value("${spring.mail.port:465}")
+    @Value("${spring.mail.port}")
     private Integer port;
 
     @Value("${spring.mail.username}")
@@ -34,27 +28,18 @@ public class MailAccountConfig {
     @Value("${spring.mail.password}")
     private String password;
 
-    @Value("${spring.mail.default-encoding:UTF-8}")
-    private String defaultEncoding;
-
     @Value("${spring.mail.properties.mail.smtp.auth:true}")
     private Boolean auth;
 
     @Value("${spring.mail.properties.mail.smtp.ssl.enable:true}")
     private Boolean sslEnable;
 
-    @Value("${spring.mail.properties.mail.smtp.ssl.required:true}")
-    private Boolean sslRequired;
-
-    @Value("${spring.mail.properties.mail.smtp.socketFactory.class:javax.net.ssl.SSLSocketFactory}")
-    private String socketFactoryClass;
-
-    @Value("${spring.mail.properties.mail.smtp.socketFactory.port:465}")
-    private Integer socketFactoryPort;
+    @Value("${spring.mail.default-encoding:UTF-8}")
+    private String charset;
 
     /**
-     * 创建 MailAccount Bean
-     * 从 Spring Boot 的邮件配置中读取并转换为 Hutool 的 MailAccount
+     * 创建 Hutool MailAccount Bean
+     * 从 Spring Boot 的邮件配置中读取参数
      */
     @Bean
     public MailAccount mailAccount() {
@@ -63,36 +48,22 @@ public class MailAccountConfig {
         // 基本配置
         account.setHost(host);
         account.setPort(port);
+        account.setFrom(username);
         account.setUser(username);
         account.setPass(password);
-        account.setFrom(username);    // 默认使用用户名作为发件人
         account.setAuth(auth);
         account.setSslEnable(sslEnable);
         
-        // 设置编码
-        try {
-            Charset charset = Charset.forName(defaultEncoding);
-            account.setCharset(charset);
-        } catch (Exception e) {
-            log.warn("无法解析编码 {}，使用默认编码 UTF-8", defaultEncoding);
-            account.setCharset(StandardCharsets.UTF_8);
+        // 如果启用了 SSL，需要设置相关属性
+        if (sslEnable) {
+            account.setStarttlsEnable(true);
+            // 设置 Socket Factory
+            account.setSocketFactoryClass("javax.net.ssl.SSLSocketFactory");
+            account.setSocketFactoryPort(port);
         }
         
-        // 设置 SMTP 属性
-        Properties props = account.getSmtpProps();
-        if (props == null) {
-            props = new Properties();
-        }
-        props.setProperty("mail.smtp.auth", String.valueOf(auth));
-        props.setProperty("mail.smtp.ssl.enable", String.valueOf(sslEnable));
-        props.setProperty("mail.smtp.ssl.required", String.valueOf(sslRequired));
-        props.setProperty("mail.smtp.socketFactory.class", socketFactoryClass);
-        props.setProperty("mail.smtp.socketFactory.port", String.valueOf(socketFactoryPort));
-        
-        log.info("MailAccount Bean 初始化成功: host={}, port={}, username={}", 
-                host, port, username);
+        log.info("Hutool MailAccount Bean 初始化成功 - Host: {}, Port: {}, User: {}", host, port, username);
         
         return account;
     }
 }
-
